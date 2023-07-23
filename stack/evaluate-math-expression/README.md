3a08e15051be1edf1f9f882a4a83e04aae17302a
drajamanthri <drajamanthri@biotech.com>
2023-07-23 13:21:16 -0700

    expressions can contain parenthesis

---
+++ stack/evaluate-math-expression/README.md
@@ -0,0 +1,10 @@
+<h1>Evaluate Expression</h1>
+Given mathematical string expression, evaluate it.
+
+<h2>Constraints</h2>
+<ul>
+    <li>The expression is given as string</li>
+    <li>parenthesis are well matched</li>
+    <li>numbers are single digit integers</li>
+    <li>no spaces</li>
+</ul>
--- stack/evaluate-math-expression/solution.py
+++ stack/evaluate-math-expression/solution.py
@@ -1,11 +1,18 @@
 from collections import deque
 
+'''
+This class represents a mathematical expression. The expression is given as a string when creating an
+object of the Expression class.
+'''
 class Expression:
     def __init__(self, expression:str):
         self.exp:str = expression
         self.operators:list = ['+', '-', '*', '/']
         self.precedence:dict = {'+': 0, '-': 0, '*': 1, '/': 1}
 
+    '''
+    This function evaluate the expression given by the constructor
+    '''
     def eval(self):
         vals = deque() #vals stack
         ops = deque()
@@ -16,6 +23,23 @@ class Expression:
             if token.isnumeric():
                 #if the token is a number, add it to the vals stack
                 vals.append(token)
+            elif token == '(':
+                # if the token is the left parenthesis push it to ops
+                ops.append(token)
+            elif token == ')':
+                last_op = ops[-1]
+                while ops and last_op != '(':
+                    op = ops.pop()
+                    val2 = vals.pop()
+                    val1 = vals.pop()
+                    result = self.apply_operator(op, val1, val2)
+                    vals.append(result)
+                    if ops:
+                        last_op = ops[-1]
+
+                if ops and last_op == '(':
+                    ops.pop()
+
             elif self.tokenIsOperator(token):
                 #if the token is an operator
                 if len(ops) == 0:
@@ -24,7 +48,7 @@ class Expression:
                 else:
                     #if the operator stack is not empty
                     last_op = ops[-1]
-                    while ops and (not self.higher_precedence(token, last_op)):
+                    while ops and last_op != '(' and (not self.higher_precedence(token, last_op)):
                         #while the current operand does not have a higher precedence, apply the stacked operators.
                         last_op = ops.pop()
                         val2 = vals.pop()
@@ -63,6 +87,13 @@ class Expression:
         else:
             return False
         
+    '''
+    @param operator:str +, -, *, /
+    @param operand1:str 
+    @param operand2:str
+    for example 
+    operand1 + operand2
+    '''    
     def apply_operator(self, operator:str, operand1:str, operand2:str)->int:
         if operator == '+':
             return int(operand1) + int(operand2)
@@ -71,8 +102,8 @@ class Expression:
         elif operator == '*':
             return int(operand1) * int(operand2)
         elif operator == '/':
-            return int(operand1) / int(operand2)
+            return int(operand1) // int(operand2)
         
-expression = Expression('2+3+4')
+expression = Expression('5+4/(1+1)')
 result = expression.eval()
 print(result)
