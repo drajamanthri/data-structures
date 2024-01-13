8f285dcbf93841d56b2aa70481962f1bf69c5650
drajamanthri <drajamanthri@biotech.com>
2024-01-13 14:45:38 -0800

    binary tree bfs level loop

---
+++ binary-tree/bfs/level-loop/README.md
@@ -0,0 +1,2 @@
+<h1>Binary Tree- Breadth First Search- Level loop</h1>
+The breadth first search algorithm is implemented by using a queue. Add a loop which pops the nodes related to the same level separately.
---
+++ binary-tree/bfs/level-loop/solution.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+class Node:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+class Tree:
+    '''
+    This function prints each node value of the binary tree given by the root by using bfs
+    '''
+    def bfs(self, root):
+        if root is None:
+            return
+
+        q = deque([root])
+        while q:
+            width = len(q)
+            #level specific logic goes here
+
+            for i in range(width): #this loop pops only the nodes related to the current level
+
+                node = q.popleft()
+                print(node.val)
+
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+
+n1.left = n2
+n1.right = n3
+
+t = Tree()
+t.bfs(n1)
+
+        
+
+    
